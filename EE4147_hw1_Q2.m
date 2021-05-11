a6e2dc0c0328e1a2092c082886bf2b168acb4594
Natasha <natashabirari@gmail.com>
2021-05-11 17:34:53 +0200

    replaced earlier version with update.

--- EE4147_hw1_Q2.m
+++ EE4147_hw1_Q2.m
@@ -4,7 +4,7 @@
 %% Question 1 (a)
 L = 3;
 syms t
-t = 0:.1:L;
+t = 0:.1:4;
 
 tres = .1;                          % time delay resolution
 tau_array = (0:tres:1-tres);        % delay values
@@ -16,30 +16,25 @@ tau = tau_array(idx);               % get random delay 'tau'
 % tau = 0.5;    % choose from 0, 0.1, ... 0.8, 0.9
 
 % choose arbitrary P, L values for now
-P = 1.5*(1/tres);
+P = 15;
 L = 3;
 
 % sample pulse with delay
-g = pulse(t,tau,L,P);
+g = pulse(tau,L,P);
 
-% new time axis
-t22 = linspace(0,3,L*P);
-% % plot over-sampled g(tau)
-% figure(1); plot(t2,g);
-% title('sampled pulse'); xlabel('t');
-% 
-% % plot reference pulse and delayed pulse
-% for k = 1
-%     tri = @(t) triangularPulse(0,2,t);
-%     figure(1);
-%     plot(t,tri(t));
-%     hold on;
-%     plot(t22,g);
-%     ylabel('g(t)'); xlabel('t');
-%     str = {'Time Delay tau',num2str(tau)};
-%     annotation('textbox',[0.2 0.5 0.3 0.3],'String',str,'FitBoxToText','on');
-%     title('reference pulse and delayed pulse');
-% end
+% plot reference pulse and delayed pulse
+t22 = linspace(0,3,L*P);        % new time axis
+for k = 1
+    tri = @(t) triangularPulse(0,2,t);
+    figure(1);
+    plot(t,tri(t));
+    hold on;
+    plot(t22,g);
+    ylabel('g(t)'); xlabel('t');
+    str = {'Time Delay tau',num2str(tau)};
+    annotation('textbox',[0.2 0.5 0.3 0.3],'String',str,'FitBoxToText','on');
+    title('reference pulse and delayed pulse');
+end
 
 %% Question 1(b)
 r = 2;                                  % arbitrary number of paths
@@ -48,41 +43,37 @@ tau_vec = tau_array(idxs);              % generate time delay vector
 
 beta = 1;                               % gains, column vector
 
-h = channel(t,tau_vec, beta, L, P);
+h = channel(tau_vec, beta, L, P);
 
 %% Question 1 (c)
 qpsk_symb = [1 1j -1 -1j];
-N = 8;
+N = 3;
 s = source(N, qpsk_symb)';
 
 %% Question 1 (d)
-x = gen_data(h,s,P,N);
+[x, xx] = gen_data(h,s,P,N);
 
-%% Question 2 - PROBLEM
-
+%% Question 2
+% given param values
 r2 = 2;
 tau2 = [0.1 0.6]';
-phi1 = 0;   % deg
-phi2 = 15;  % deg
-beta2 = [1*exp(1j*deg2rad(phi1)) 0.7*exp(1j*deg2rad(phi2))]';
+phi = [0 15];       % arbitrary, deg
+beta2 = [1*exp(1j*deg2rad(phi(1))) 0.7*exp(1j*deg2rad(phi(2)))]';
 P2 = 5;
 N2 = 50;
-
-t2 = linspace(1,N2,N2);
-L2 = N2+2;
-
-h2 = channel(t2,tau2, beta2, L2, P2)';
-
-s2 = source(N2, qpsk_symb)';
-
-[x2, xx2] = gen_data(h2,s2,P2,N2);
+% derived values
+t2 = linspace(0,N2-1,N2);
+L2 = N2+1;
+h2 = channel(tau2, beta2, L2, P2)'; % channel
+s2 = source(N2, qpsk_symb)';        % source
+[x2, xx2] = gen_data(h2,s2,P2,N2);  % data
 
 % plot results
 lenx = length(x2);
 t_ax2 = linspace(0,lenx+1,lenx);
 figure(2); plot(t_ax2, real(x2)); hold on; plot(t_ax2, imag(x2))
 legend('real','imag');title('real and imaginary parts of x2')
-xlim([0 300]);
+xlim([0 70]);
 
 lenh = length(h2);
 t_ax3 = linspace(0,lenh+1,lenh);
@@ -90,12 +81,20 @@ figure(3); plot(t_ax3, real(h2)); hold on; plot(t_ax3, imag(h2))
 legend('real','imag');title('real and imaginary parts of h2')
 xlim([0 15]);
 
+fprintf('-------------------\n');
+fprintf('Source s2 length: %d\n', length(s2));
+fprintf('Channel h2 length: %d\n', length(h2));
+fprintf('Convolution output xx2 length: %d\n', length(xx2));
+fprintf('Truncated data vector x2 length: %d\n', length(x2));
+
 %% Question 3
+%  reshape
 X = reshape(x2,P2,[]);
+rX = rank(X);
 
 %% Functions
 
-function g = pulse(t,tau, L, P)
+function g = pulse(tau, L, P)
     % Sample Pulse With Delay
     % INPUT:
     % tau : time delay
@@ -112,12 +111,9 @@ function g = pulse(t,tau, L, P)
     for k=0:(P*L - 1)
             g(k+1) = tri(k*1/P - tau);
     end
-    
-    % Alternative
-    %g = resample(tri(t-tau),P,1);
 end
 
-function h = channel(t,tau_vec, beta, L, P)
+function h = channel(tau_vec, beta, L, P)
     % Channel Response With Delays
     % INPUT:
     % tau_vec : time delays [tau_1 ... tau_r]
@@ -129,14 +125,13 @@ function h = channel(t,tau_vec, beta, L, P)
     % h : sampled channel
     
     r = length(beta);
-    T = length(t);
     
     for m = 1:r
         % generate g(t - tau) and h(t)
         tau = tau_vec(m);
         b = beta(m);
         
-        g = pulse(t,tau, L, P);
+        g = pulse(tau, L, P);
         
         h_vec(m,:) = b*g;     
     end
@@ -156,9 +151,8 @@ function [x, xx] = gen_data(h,s,P,N)
     s_ext = kron(s,[1;zeros(P-1,1)]);
     xx = conv(h,s);
 
-% sample
-    x = resample(xx,P,1);
-    %x = xx(1:1/P:(P*N - 1)*1/P);
+    % sample
+    x = xx(1:N*P);
 end
 
 
